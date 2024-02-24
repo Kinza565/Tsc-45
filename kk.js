ad5689a956b13bc706cf979dbbb2efcd7df9e5ba
Kinza565 <160307961+Kinza565@users.noreply.github.com>
2024-02-24 21:19:45 +0500

    Questions
    45 Assignment

--- kk.js
+++ kk.js
@@ -1,7 +1,5 @@
-// lower case
-var personName = "Kinza";
-console.log("lowercase:", personName.toLowerCase());
-// upper case
-console.log("uppercase:", personName.toLocaleUpperCase());
-// Tittle case
-console.log("titlecase:", personName.replace(/\kw/g, function (c) { return c.toUpperCase(); }));
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var kk_1 = require("../Q14,15.16,17/kk");
+console.log("n PRINTING NUMBER OF GUEST INVITED");
+console.log("we had finally invited ".concat(kk_1.guest_list.length, " from question 14"));
